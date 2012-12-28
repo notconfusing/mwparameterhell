e2bc973ec70e881425f4445eb0c8c1a165ac2142
notconfusing <isalix@gmail.com>
2012-12-28 11:40:40 -0800

    using SIGALARM for timeout checkout

--- no_math.py
+++ no_math.py
@@ -26,29 +26,47 @@ def deMath(pagetext):
         pagetext = pagetext[:mathExpression.start()] + pagetext[mathExpression.end():]
     return pagetext
 
+
      
 def parseALump(lumpnum):
     lumplocation = configurations.lumpDirectory + '/enwiki_lumped_' + str(lumpnum) + '.xml'
+    templocation = open(configurations.tempDirectory + '/enwiki_lumped_' + str(lumpnum) + '.xml', 'w+')
     savelumplocation = open(configurations.saveLumpDirectory + '/enwiki_lumped_' + str(lumpnum) + '.xml', 'w+')
     try:
         tree = ET.parse(lumplocation)
     except ET.ParseError:
         print 'OH FUCK'
+        return
     root = tree.getroot()
-
+    
     for page in root.iter('{http://www.mediawiki.org/xml/export-0.7/}page'):
         for revision in page.iter('{http://www.mediawiki.org/xml/export-0.7/}revision'):
             for text in revision.iter('{http://www.mediawiki.org/xml/export-0.7/}text'):
-                deMathedText = deMath(text.text)
-            
-
-    tree.write(savelumplocation)
+                before1 = len(text.text)
+                text.text = deMath(text.text)
+                after1 = len(text.text)
+                if not before1==after1:
+                    text.set('updated', 'yes')
+                    
+    tree.write(templocation)
+    templocation.close()
+    
+    #now convert the ns0:'s out
+    inFile = open(configurations.tempDirectory + '/enwiki_lumped_' + str(lumpnum) + '.xml', 'r')
+    lines = inFile.readlines()
+    for line in lines:
+        outLine = line.replace('ns0:','')
+        savelumplocation.write(outLine)
     savelumplocation.close()
+    
+
+
     
 
 #make a list of jobs and run and wait for them
 jobs = []
-for i in range(1,configurations.cores+1):
+for i in range(26,27):
+#for i in range(1,configurations.cores+1):
     proc = multiprocessing.Process(target=parseALump, args=(i,))
     jobs.append(proc)
 
